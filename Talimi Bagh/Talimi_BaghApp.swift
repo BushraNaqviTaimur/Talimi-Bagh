78220f8d83feb6dfe8cdc47899247dbc4378a454
BushraNaqviTaimur <syedabushranaqvi@icloud.com>
2022-08-30 11:28:18 +0500

    Initial Commit

---
+++ Talimi Bagh/Talimi_BaghApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talimi_BaghApp.swift
+//  Talimi Bagh
+//
+//  Created by Bushra on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Talimi_BaghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
